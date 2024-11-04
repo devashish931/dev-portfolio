34688c25d3a9d11a31bf41662a1acd1ebbb39d9a
Devashish Trehan <devashish931.dk@gmail.com>
2024-11-04 12:19:35 +0530

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head />
-      <body className="text-gray-400">
+      <body>
         <Main />
         <NextScript />
       </body>
